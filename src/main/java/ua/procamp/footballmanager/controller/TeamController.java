64038fc9f53c2c8e8cc0a7971d05a97e08719a50
shtramak <shtramak@gmail.com>
2019-05-03 19:03:43 +0300

    implement TeamController with all required endpoints

--- src/main/java/ua/procamp/footballmanager/controller/TeamController.java
+++ src/main/java/ua/procamp/footballmanager/controller/TeamController.java
@@ -1,9 +1,17 @@
 package ua.procamp.footballmanager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import ua.procamp.footballmanager.entity.Team;
+import ua.procamp.footballmanager.dto.PlayerDto;
+import ua.procamp.footballmanager.dto.TeamDto;
 import ua.procamp.footballmanager.service.TeamService;
 
 import java.util.List;
@@ -18,7 +26,41 @@ public class TeamController {
     }
 
     @GetMapping
-    public List<Team> findAll(){
+    public List<TeamDto> findAll() {
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TeamDto findById(@PathVariable Long id) {
+        return service.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TeamDto save(@RequestBody TeamDto teamDto) {
+        return service.save(teamDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remove(@PathVariable Long id) {
+        service.removeById(id);
+    }
+
+    @GetMapping("/{id}/captain")
+    public PlayerDto findCaptain(@PathVariable("id") Long id) {
+        return service.findCaptainByTeam(id);
+    }
+
+    @PutMapping("/{teamId}/captain/{playerId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void assignCaptain(@PathVariable("teamId") Long teamId, @PathVariable("playerId") Long playerId) {
+        service.assignCaptainByTeam(teamId, playerId);
+    }
+
+    @PostMapping("/{id}/player")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addNewPlayerToTeam(@PathVariable Long id, @RequestBody PlayerDto playerDto) {
+        service.addNewPlayerToTeam(id, playerDto);
     }
 }
