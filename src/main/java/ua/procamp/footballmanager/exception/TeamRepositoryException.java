2b3f72b46a9f0d6cd7a93641e18692a7fecc3b42
shtramak <shtramak@gmail.com>
2019-05-04 12:11:14 +0300

    rename TeamRepositoryException to EntityNotFoundException

--- src/main/java/ua/procamp/footballmanager/exception/TeamRepositoryException.java
+++
@@ -1,7 +0,0 @@
-package ua.procamp.footballmanager.exception;
-
-public class TeamRepositoryException extends RuntimeException {
-    public TeamRepositoryException(String message) {
-        super(message);
-    }
-}
