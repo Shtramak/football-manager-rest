4cd9e02657d7bd2752bafbed394cec07c1aa0ac4
shtramak <shtramak@gmail.com>
2019-05-21 13:46:21 +0300

    update PlayerServiceImplTest class. Cover all methods with unit tests

--- src/test/java/ua/procamp/footballmanager/dto/PlayerMapperTest.java
+++ src/test/java/ua/procamp/footballmanager/dto/PlayerMapperTest.java
@@ -10,15 +10,15 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static ua.procamp.footballmanager.TestUtils.generatePlayerWithId;
+import static ua.procamp.footballmanager.TestUtils.generatePlayerWithIdAndNoTeam;
 
 class PlayerMapperTest {
 
     @Test
     void playerToPlayerDtoWithoutTeamReturnsPlayerDtoWithNullTeamName() {
-        Player player = generatePlayerWithId(1L);
+        Player player = generatePlayerWithIdAndNoTeam(1);
         PlayerDto playerDto = new PlayerDto();
-        setPlayerFeildsToPlayerDtoFields(player, playerDto);
+        setPlayerFieldsToPlayerDtoFields(player, playerDto);
         PlayerDto actual = PlayerMapper.playerToPlayerDto(player);
         PlayerMapper.playerToPlayerDto(player);
         assertThat(playerDto, samePropertyValuesAs(actual));
@@ -26,12 +26,12 @@ class PlayerMapperTest {
 
     @Test
     void playerToPlayerDtoWithoutTeamReturnsPlayerDtoWithTeamName() {
-        Player player = generatePlayerWithId(1L);
+        Player player = generatePlayerWithIdAndNoTeam(1);
         Team team = new Team();
         team.setName("testTeam");
         player.setTeam(team);
         PlayerDto playerDto = new PlayerDto();
-        setPlayerFeildsToPlayerDtoFields(player, playerDto);
+        setPlayerFieldsToPlayerDtoFields(player, playerDto);
         PlayerDto actual = PlayerMapper.playerToPlayerDto(player);
         PlayerMapper.playerToPlayerDto(player);
         assertThat(playerDto, samePropertyValuesAs(actual));
@@ -39,25 +39,25 @@ class PlayerMapperTest {
 
     @Test
     void playerDtoToPlayerWithoutTeam() {
-        Player player = generatePlayerWithId(1L);
+        Player player = generatePlayerWithIdAndNoTeam(1);
         PlayerDto playerDto = new PlayerDto();
-        setPlayerFeildsToPlayerDtoFields(player, playerDto);
+        setPlayerFieldsToPlayerDtoFields(player, playerDto);
         Player actual = PlayerMapper.playerDtoToPlayer(playerDto);
         assertThat(player, samePropertyValuesAs(actual));
     }
 
     @Test
     void listPlayerToListPlayerDto() {
-        Player player1 = generatePlayerWithId(1L);
-        Player player2 = generatePlayerWithId(2L);
-        Player player3 = generatePlayerWithId(3L);
+        Player player1 = generatePlayerWithIdAndNoTeam(1);
+        Player player2 = generatePlayerWithIdAndNoTeam(2);
+        Player player3 = generatePlayerWithIdAndNoTeam(3);
         List<Player> players = List.of(player1, player2, player3);
         PlayerDto playerDto1 = new PlayerDto();
         PlayerDto playerDto2 = new PlayerDto();
         PlayerDto playerDto3 = new PlayerDto();
-        setPlayerFeildsToPlayerDtoFields(player1, playerDto1);
-        setPlayerFeildsToPlayerDtoFields(player2, playerDto2);
-        setPlayerFeildsToPlayerDtoFields(player3, playerDto3);
+        setPlayerFieldsToPlayerDtoFields(player1, playerDto1);
+        setPlayerFieldsToPlayerDtoFields(player2, playerDto2);
+        setPlayerFieldsToPlayerDtoFields(player3, playerDto3);
         List<PlayerDto> playersDto = List.of(playerDto1, playerDto2, playerDto3);
         List<PlayerDto> actual = PlayerMapper.listPlayerToListPlayerDto(players);
         assertEquals(playersDto.size(), actual.size());
@@ -68,16 +68,16 @@ class PlayerMapperTest {
 
     @Test
     void listPlayerDtoToListPlayer() {
-        Player player1 = generatePlayerWithId(1L);
-        Player player2 = generatePlayerWithId(2L);
-        Player player3 = generatePlayerWithId(3L);
+        Player player1 = generatePlayerWithIdAndNoTeam(1);
+        Player player2 = generatePlayerWithIdAndNoTeam(2);
+        Player player3 = generatePlayerWithIdAndNoTeam(3);
         List<Player> players = List.of(player1, player2, player3);
         PlayerDto playerDto1 = new PlayerDto();
         PlayerDto playerDto2 = new PlayerDto();
         PlayerDto playerDto3 = new PlayerDto();
-        setPlayerFeildsToPlayerDtoFields(player1, playerDto1);
-        setPlayerFeildsToPlayerDtoFields(player2, playerDto2);
-        setPlayerFeildsToPlayerDtoFields(player3, playerDto3);
+        setPlayerFieldsToPlayerDtoFields(player1, playerDto1);
+        setPlayerFieldsToPlayerDtoFields(player2, playerDto2);
+        setPlayerFieldsToPlayerDtoFields(player3, playerDto3);
         List<PlayerDto> playersDto = List.of(playerDto1, playerDto2, playerDto3);
         List<Player> actual = PlayerMapper.listPlayerDtoToListPlayer(playersDto);
         assertEquals(playersDto.size(), actual.size());
@@ -86,7 +86,7 @@ class PlayerMapperTest {
         }
     }
 
-    private void setPlayerFeildsToPlayerDtoFields(Player player, PlayerDto playerDto) {
+    private void setPlayerFieldsToPlayerDtoFields(Player player, PlayerDto playerDto) {
         playerDto.setPlayerId(player.getId());
         playerDto.setFirstName(player.getFirstName());
         playerDto.setLastName(player.getLastName());
--- src/test/java/ua/procamp/footballmanager/service/PlayerServiceImplTest.java
+++ src/test/java/ua/procamp/footballmanager/service/PlayerServiceImplTest.java
@@ -1,6 +1,5 @@
 package ua.procamp.footballmanager.service;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,6 +8,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import ua.procamp.footballmanager.dto.PlayerDto;
 import ua.procamp.footballmanager.dto.PlayerMapper;
 import ua.procamp.footballmanager.entity.Player;
+import ua.procamp.footballmanager.exception.EntityNotFoundException;
 import ua.procamp.footballmanager.repository.PlayerRepository;
 
 import java.util.Collections;
@@ -16,12 +16,15 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
-import static ua.procamp.footballmanager.TestUtils.generatePlayerWithId;
+import static ua.procamp.footballmanager.TestUtils.generatePlayerWithIdAndNoTeam;
 
 @ExtendWith(MockitoExtension.class)
 class PlayerServiceImplTest {
@@ -38,12 +41,12 @@ class PlayerServiceImplTest {
 
     @Test
     void findAllWhenRepoHasDataReturnsListOfPlayers() {
-        List<Player> expected = List.of(generatePlayerWithId(1L), generatePlayerWithId(2L));
+        List<Player> expected = List.of(generatePlayerWithIdAndNoTeam(1L), generatePlayerWithIdAndNoTeam(2L));
         when(repository.findAll()).thenReturn(expected);
         verifyNoMoreInteractions(repository);
         List<PlayerDto> players = service.findAll();
         List<Player> actual = PlayerMapper.listPlayerDtoToListPlayer(players);
-        assertThat(actual, Matchers.containsInAnyOrder(expected.toArray()));
+        assertThat(actual, containsInAnyOrder(expected.toArray()));
     }
 
     @Test
@@ -59,7 +62,7 @@ class PlayerServiceImplTest {
     @Test
     void findByIdWhenPlayerExistsReturnsOptionalWithPlayer() {
         long playerId = 1L;
-        Player expected = generatePlayerWithId(playerId);
+        Player expected = generatePlayerWithIdAndNoTeam(playerId);
         when(repository.findById(playerId)).thenReturn(Optional.of(expected));
         verifyNoMoreInteractions(repository);
         Player actual = service.findById(playerId)
@@ -80,10 +83,11 @@ class PlayerServiceImplTest {
     @Test
     void saveNotExistingPlayerReturnPlayerWithId() {
         Long playerId = 1L;
-        Player playerWithoutId = generatePlayerWithId(playerId);
+        Player playerWithoutId = generatePlayerWithIdAndNoTeam(playerId);
         playerWithoutId.setId(null);
-        Player expected = generatePlayerWithId(playerId);
+        Player expected = generatePlayerWithIdAndNoTeam(playerId);
         when(repository.save(playerWithoutId)).thenReturn(expected);
+        verifyNoMoreInteractions(repository);
         PlayerDto playerDtoWithoutId = PlayerMapper.playerToPlayerDto(playerWithoutId);
         PlayerDto playerDtoWithId = service.save(playerDtoWithoutId);
         Player actual = PlayerMapper.playerDtoToPlayer(playerDtoWithId);
@@ -93,11 +97,68 @@ class PlayerServiceImplTest {
     @Test
     void savePlayerWithIdThrowsIllegalStateException() {
         long playerId = 1L;
-        Player playerWithId = generatePlayerWithId(playerId);
+        Player playerWithId = generatePlayerWithIdAndNoTeam(playerId);
         PlayerDto playerDtoWithId = PlayerMapper.playerToPlayerDto(playerWithId);
         IllegalStateException exception = assertThrows(IllegalStateException.class, () -> service.save(playerDtoWithId));
         String message = String.format("You're trying to save an existing player with id=%d..." +
                 "If it is exactly what you want - pleas use update request", playerId);
-        assertEquals(message, exception.getMessage());
+        assertThat(exception.getMessage(), equalTo(message));
+    }
+
+    @Test
+    void saveWhenPlayerDtoIsNullThrowsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> service.save(null), "service.save(null) should throw NPE");
+    }
+
+    @Test
+    void updateWhenPlayerExistsUpdatesExistingPlayer() {
+        long playerId = 1L;
+        Player existingPlayer = generatePlayerWithIdAndNoTeam(playerId);
+        when(repository.findById(playerId)).thenReturn(Optional.of(existingPlayer));
+        verifyNoMoreInteractions(repository);
+        Player updatedPlayer = generatePlayerWithIdAndNoTeam(playerId);
+        updatedPlayer.setFirstName("updated FirstName");
+        PlayerDto updatedPlayerDto = PlayerMapper.playerToPlayerDto(updatedPlayer);
+        service.update(updatedPlayerDto);
+        assertThat(existingPlayer, samePropertyValuesAs(updatedPlayer));
+    }
+
+    @Test
+    void updateWhenPlayerNotExistsThrowsEntityNotFoundException() {
+        long notExistedId = 1L;
+        when(repository.findById(notExistedId)).thenReturn(Optional.empty());
+        verifyNoMoreInteractions(repository);
+        Player player = generatePlayerWithIdAndNoTeam(notExistedId);
+        PlayerDto playerDto = PlayerMapper.playerToPlayerDto(player);
+        EntityNotFoundException exception =
+                assertThrows(EntityNotFoundException.class, () -> service.update(playerDto));
+        String message = String.format("Player with id %d not found", playerDto.getPlayerId());
+        assertThat(message, equalTo(exception.getMessage()));
+    }
+
+    @Test
+    void updateWhenPlayerDtoIsNullThrowsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> service.update(null), "service.update(null) should throw NPE");
+    }
+
+    @Test
+    void removeByIdInvokeRepositoryDeleteById() {
+        long playerId = 1L;
+        service.removeById(playerId);
+        verify(repository, times(1)).deleteById(playerId);
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    void findPlayerByTeamWhenTeamWithPlayerExists() {
+        Player player1 = generatePlayerWithIdAndNoTeam(1L);
+        Player player2 = generatePlayerWithIdAndNoTeam(2L);
+        Player player3 = generatePlayerWithIdAndNoTeam(3L);
+        List<Player> players = List.of(player1, player2, player3);
+        long teamId = 1L;
+        when(repository.findByTeam(teamId)).thenReturn(players);
+        List<PlayerDto> playersDto = service.findPlayersByTeam(teamId);
+        List<Player> actual = PlayerMapper.listPlayerDtoToListPlayer(playersDto);
+        assertThat(actual, containsInAnyOrder(players.toArray()));
     }
 }
