a1a3fa3770bf70b9fb253db72c1f8e60f961f813
shtramak <shtramak@gmail.com>
2019-05-30 13:54:45 +0300

    update TeamRepository findAll() with JPQL to avoid redundant queries

--- src/main/java/ua/procamp/footballmanager/repository/TeamRepository.java
+++ src/main/java/ua/procamp/footballmanager/repository/TeamRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.repository.query.Param;
 import ua.procamp.footballmanager.entity.Player;
 import ua.procamp.footballmanager.entity.Team;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long>, CustomTeamRepository {
-    @Query("SELECT t.captain from Team t where t.id=:id")
+    @Query("SELECT t.captain FROM Team t WHERE t.id=:id")
     Optional<Player> findCaptain(@Param("id") long id);
+
+    @Query("SELECT DISTINCT t FROM Team t LEFT JOIN FETCH t.players left join fetch t.captain")
+    List<Team> findAll();
 }
